1520591638db38a13f7753a57071b1749a1c22e8
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-28 21:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let body=document.getElementsByTagName("BODY")[0];
+window.addEventListener("resize",resize);
+function resize() {
+	body.textContent=`Width: ${window.outerWidth} and Height : ${window.outerHeight}`;
+}
+resize();
